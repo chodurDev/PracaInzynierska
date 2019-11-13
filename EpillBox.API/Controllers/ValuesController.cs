8cbfba82fb546de74f2ffaa1806408ef2c9163fb
kamil.chodurek <kamil.chodurek@KRK1-LDL-P90754>
2019-11-13 18:30:10 +0100

    dodany widok do rejestracji  oraz zapisywanie uzytkownika do bazy

--- EpillBox.API/Controllers/ValuesController.cs
+++ EpillBox.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace EpillBox.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
